44234569ffa9d02d91ef6dcedcb3b001b3e7f8e9
Chris Cooper <christopher_cooper@harvard.edu>
2017-02-14 21:36:06 -0500

    seems to work

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"strconv"
+
+	"reflect"
 
 	"gopkg.in/yaml.v2"
 )
@@ -26,17 +29,18 @@ func main() {
 		fmt.Fprintln(os.Stderr, "Missing required argument -f")
 	}
 
-	var i interface{}
+	var m map[interface{}]interface{}
+
 	data, err := ioutil.ReadFile(inFile)
 	if err != nil {
 		exitError(err)
 	}
-	err = yaml.Unmarshal(data, &i)
-	if err != nil {
-		exitError(err)
-	}
+	err = yaml.Unmarshal(data, &m)
 
-	data, err = json.Marshal(i)
+	nm := make(map[string]interface{})
+	recurseMapInterface(m, nm)
+
+	data, err = json.MarshalIndent(nm, "", "	")
 	if err != nil {
 		exitError(err)
 	}
@@ -47,6 +51,75 @@ func main() {
 		fmt.Print(string(data))
 	}
 
+}
+
+func recurseMapInterface(m map[interface{}]interface{}, newMap map[string]interface{}) {
+	nm := make(map[string]interface{})
+	for k, v := range m {
+		nk := ""
+		switch t := k.(type) {
+		case string:
+			nk = t
+		case int:
+			nk = strconv.Itoa(t)
+		case float64:
+			nk = strconv.FormatFloat(t, 'E', -1, 64)
+		case map[interface{}]interface{}:
+			recurseMapInterface(t, newMap)
+		}
+
+		var nv interface{}
+		switch t := v.(type) {
+		case map[interface{}]interface{}:
+			m := make(map[string]interface{})
+			nm[nk] = m
+			recurseMapInterface(t, m)
+		case []interface{}:
+			recurseArray(nk, t, nm)
+		default:
+			nv = t
+		}
+		if nv != nil {
+			nm[nk] = nv
+		}
+	}
+
+	for k, v := range nm {
+		newMap[k] = v
+	}
+
+}
+
+func recurseArray(k string, slc []interface{}, container interface{}) {
+	nslc := make([]interface{}, 0)
+	for _, i := range slc {
+		var v interface{}
+		switch i := i.(type) {
+		case []interface{}:
+			recurseArray(k, i, &nslc)
+		case map[interface{}]interface{}:
+			m := make(map[string]interface{})
+			nslc = append(nslc, m)
+			recurseMapInterface(i, m)
+		default:
+			v = i
+		}
+		if v != nil {
+			nslc = append(nslc, v)
+		}
+	}
+
+	rv := reflect.ValueOf(container)
+	if rv.Kind() == reflect.Ptr {
+		rv = reflect.Indirect(rv)
+	}
+	switch rv.Kind() {
+	case reflect.Slice:
+		*container.(*[]interface{}) = append(*container.(*[]interface{}), nslc)
+	case reflect.Map:
+		m := container.(map[string]interface{})
+		m[k] = nslc
+	}
 }
 
 func exitError(err error) {
